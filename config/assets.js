04cfea526b691b2fa1f33cab321bee1a229ed1db
Ege <ege.yildir@hotmail.com>
2021-02-12 17:53:58 +0300

    Add assets file

---
+++ config/assets.js
@@ -0,0 +1,4 @@
+const assets = {
+    backgroundImage: "http://images.unsplash.com/photo-1517021818302-9b520a06c834?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+}
+export default assets;
