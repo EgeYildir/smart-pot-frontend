49ae76db0277ff3d3ba9f72edf5246329d2c2342
Ege <ege.yildir@hotmail.com>
2021-03-06 20:35:33 +0300

    Add authentication system to App.js

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
+import AppLoading from 'expo-app-loading'
 import AuthNavigator from './navigation/AuthNavigator';
 import TabNavigator from './navigation/TabNavigator';
+import AuthContext from './auth/context';
+import authStorage from './auth/storage';
 
 export default function App() {
+  const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
+
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
+  }
+
+  /*if (!isReady){
+    return (<AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} onError={console.log('Error loading the app.')} />);
+  };*/
+
   return (
-    <NavigationContainer style={styles.container}>
-      {/* <AuthNavigator /> */} 
-      <TabNavigator />
-    </NavigationContainer>
+    <AuthContext.Provider value={{ user, setUser }} >
+      <NavigationContainer style={styles.container}>
+        {user ? <TabNavigator /> : <AuthNavigator />}
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 }
 
