dab2596e50ec8b1e2604f65309ca7b1895768d3a
Ege <ege.yildir@hotmail.com>
2021-03-06 20:24:54 +0300

    Resolve error in useApi

--- hooks/useApi.js
+++ hooks/useApi.js
@@ -1,20 +1,23 @@
 import { useState } from 'react'
 
-export default useApi = (apiFunc) => {
+//Custom hook for data operations.
+export default function useApi(apiFunc){
     const [data, setData] = useState([]);
     const [error, setError] = useState(false);
     const [loading, setLoading] = useState(false);
 
     const request = async (...args) => {
+        //Set loading screen till you get response.
         setLoading(true);
         const response = await apiFunc(...args);
         setLoading(false);
 
+        //If response is not ok, show error.
         if(!response.ok) return setError(true);
 
         setError(false);
         setData(response.data);
     };
 
-    return { data, error, loading, request }
-}
+    return { data, error, loading, request };
+};
