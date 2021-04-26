cdbf0b96aa947f932e6489a6d53073363aca42e0
Ege <ege.yildir@hotmail.com>
2021-04-26 16:07:38 +0300

    Replace SecureStorage with AsyncStorage

--- auth/storage.js
+++ auth/storage.js
@@ -1,12 +1,14 @@
 import * as SecureStore from 'expo-secure-store'
 import jwtDecode from 'jwt-decode'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const key = "authToken";//TODO: Refactor before deployment.
 
+//TODO: Make it SecureStore before deployment.
 //Stores the authentication token in the SecureStore.
 const storeToken = async (authToken) => {
     try {
-        await SecureStore.setItemAsync(key, authToken);
+        await AsyncStorage.setItem(key, authToken);
     } catch (error) {
         console.log("Error storing the auth token", error);
     }
@@ -15,7 +17,7 @@ const storeToken = async (authToken) => {
 //Gets the authentication token from SecureStore.
 const getToken = async () => {
     try {
-        return await SecureStore.getItemAsync(key);
+        return await AsyncStorage.getItem(key);
     } catch (error) {
         console.log("Error getting the auth token", error);
     }
@@ -24,13 +26,13 @@ const getToken = async () => {
 //Decodes the token into user object. 
 const getUser = async () => {
     const token = await getToken();
-    return (token) ? jwtDecode (token) : null;
+    return (token) ? jwtDecode(token) : null;
 }
 
 //Removes token from the SecureStore.
 const removeToken = async () => {
     try {
-        await SecureStore.deleteItemAsync(key);
+        await AsyncStorage.removeItem(key);
     } catch (error) {
         console.log("Error removing the auth token", error);
     }
